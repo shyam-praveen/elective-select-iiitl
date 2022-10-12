df5dfb0c0f4e7a3b3299f1fac27757e933fa24ea
yash-saaspayapp <codemaninfinity17@gmail.com>
2022-10-12 19:42:13 +0530

    Home page design complete. Content Changes?

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
+import { NotificationsProvider } from '@mantine/notifications'
+
+import { AppHeader } from '../components/app-header'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useLocalStorage({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  })
+
+  function toggleColorScheme(value) {
+    return setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+  }
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]])
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <AppHeader
+            links={[
+              { link: '/', label: 'Home' },
+              { link: '/profile', label: 'Profile' },
+              { link: '/edit', label: 'Edit' },
+              { link: '/share', label: 'Share' },
+              { link: '/retrieve', label: 'Retrieve' },
+              { link: '/access', label: 'Access' },
+              { link: '/marketplace', label: 'Marketplace' },
+            ]}
+          />
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
 
 export default MyApp
