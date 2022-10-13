b6a94ba74d4e71082cc80e13a578da6a3d7e8d3d
yash-saaspayapp <codemaninfinity17@gmail.com>
2022-10-13 09:17:58 +0530

    profile page

---
+++ pages/profile.js
@@ -0,0 +1,36 @@
+import { Card, Avatar, Text, Button, Badge, Group, ActionIcon } from '@mantine/core'
+import { useState } from 'react'
+
+export default function Profile() {
+  const [login, setLogin] = useState(false)
+
+  return (
+    <div
+      style={{
+        maxWidth: 320,
+        margin: 'auto',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        gap: '3rem',
+      }}
+    >
+      {!login ? (
+        <Button onClick={() => setLogin(true)}>Login with google</Button>
+      ) : (
+        <Card withBorder radius="md">
+          <Group position="apart">
+            <Badge>lci2020043@iiitl.ac.in</Badge>
+          </Group>
+
+          <Text size="lg" weight={500} mt="md">
+            Student Name
+          </Text>
+          <Text size="sm" color="dimmed" mt={5}>
+            Instructions regarding elective selection.
+          </Text>
+        </Card>
+      )}
+    </div>
+  )
+}
